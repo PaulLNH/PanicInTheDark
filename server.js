a947e3e79a8654df92ed74a91c5d0a704ddbe6e5
corey decato <corey.decato@richmond.edu>
2018-07-03 17:12:48 -0400

    updating

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 var PORT = 2000;
+var Game = require('./models/game.js');
+
+Game.gamePlay();
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
@@ -9,7 +13,7 @@ app.get('/', function (req, res) {
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use('/models', express.static(__dirname + '/models'));
+// app.use('/models', express.static(__dirname + '/models'));
 
 serv.listen(PORT);
 console.log(`server listening on port ${PORT}`);
@@ -18,62 +22,66 @@ console.log(`server listening on port ${PORT}`);
 // Look into https://socket.io/get-started/chat/ for multiplayer if Firebase does not work out
 var io = require('socket.io')(serv, {});
 
+
 var SOCKET_LIST = {};
 var PLAYER_LIST = {};
 
-var Player = function(id){
-	var self = {
-		x:250,
-		y:250,
-		id:id,
-		number:"" + Math.floor(10 * Math.random()),
-		pressingRight:false,
-		pressingLeft:false,
-		pressingUp:false,
-		pressingDown:false,	
-		maxSpd:10,
-    }
-	self.updatePosition = function(){
-		if(self.pressingRight)
-			self.x += self.maxSpd;
-		if(self.pressingLeft)
-			self.x -= self.maxSpd;
-		if(self.pressingUp)
-			self.y -= self.maxSpd;
-		if(self.pressingDown)
-			self.y += self.maxSpd;
-	}
-	return self;
-}
+// var Player = function (id) {
+//     var self = {
+//         x: 250,
+//         y: 250,
+//         id: id,
+//         number: "" + Math.floor(10 * Math.random()),
+//         pressingRight: false,
+//         pressingLeft: false,
+//         pressingUp: false,
+//         pressingDown: false,
+//         maxSpd: 10,
+//     }
+//     self.updatePosition = function () {
+//         if (self.pressingRight)
+//             self.x += self.maxSpd;
+//         if (self.pressingLeft)
+//             self.x -= self.maxSpd;
+//         if (self.pressingUp)
+//             self.y -= self.maxSpd;
+//         if (self.pressingDown)
+//             self.y += self.maxSpd;
+//     }
+//     return self;
+// }
 
 io.on('connection', function (socket) {
     console.log('a user connected');
     socket.id = Math.random();
     console.log(socket.id)
-	SOCKET_LIST[socket.id] = socket;
+    SOCKET_LIST[socket.id] = socket;
 
-	var player = Player(socket.id);
-	PLAYER_LIST[socket.id] = player;
-	
-	socket.on('disconnect',function(){
-		delete SOCKET_LIST[socket.id];
-		delete PLAYER_LIST[socket.id];
-	});
-	
-	socket.on('keyPress',function(data){
-		if(data.inputId === 'left')
-			player.pressingLeft = data.state;
-		else if(data.inputId === 'right')
-			player.pressingRight = data.state;
-		else if(data.inputId === 'up')
-			player.pressingUp = data.state;
-		else if(data.inputId === 'down')
-			player.pressingDown = data.state;
+    console.log(Game.gamePlay.test);
+
+    var player = Game.Player("Player1", socket.id, 50, 50);
+    PLAYER_LIST[socket.id] = player;
+
+    socket.on('disconnect', function () {
+        delete SOCKET_LIST[socket.id];
+        delete PLAYER_LIST[socket.id];
     });
+
+    player.movement();
+    // socket.on('keyPress', function (data) {
+    //     if (data.inputId === 'left')
+    //         player.pressingLeft = data.state;
+    //     else if (data.inputId === 'right')
+    //         player.pressingRight = data.state;
+    //     else if (data.inputId === 'up')
+    //         player.pressingUp = data.state;
+    //     else if (data.inputId === 'down')
+    //         player.pressingDown = data.state;
+    // });
     // console.log(SOCKET_LIST);
     console.log(PLAYER_LIST);
     socket.on('disconnect', function () {
         console.log('user disconnected');
         console.log(socket.id);
-    });	
+    });
 });
