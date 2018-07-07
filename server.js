2cf96e5278a41d533d35b443e95d430ba19f60f7
plaird730 <plaird730@gmail.com>
2018-07-07 18:57:57 -0400

    Fixed movement, Fixed boundry collision, Added 2d map array collision, Refactored code, Fixed sizing issue with clearing canvas, Added a frames variable, Added spacing to login screen for better visual

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 console.log(`File loaded: ../server.js`);
-const test = require('./models/test.js');
+const test = require("./models/test.js");
 
 var Player = test.Player;
-var Bullet = test.Bullet;
 var SOCKET_LIST = test.SOCKET_LIST;
-var Entity = test.Entity;
 var DEBUG = test.DEBUG;
 var USERS = test.USERS;
 var isValidPassword = test.isValidPassword;
@@ -13,85 +11,79 @@ var addUser = test.addUser;
 
 var frames = 30;
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const serv = require('http').Server(app);
+const serv = require("http").Server(app);
 const PORT = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
 serv.listen(PORT);
 console.log(`Server started. localhost:${PORT}`);
 
-var io = require('socket.io')(serv, {});
-io.sockets.on('connection', function (socket) {
-    socket.id = Math.random();
-    SOCKET_LIST[socket.id] = socket;
+var io = require("socket.io")(serv, {});
+io.sockets.on("connection", function(socket) {
+  socket.id = Math.random();
+  SOCKET_LIST[socket.id] = socket;
 
-    socket.on('signIn', function (data) {
-        isValidPassword(data, function (res) {
-            if (res) {
-                Player.onConnect(socket);
-                socket.emit('signInResponse', {
-                    success: true
-                });
-            } else {
-                socket.emit('signInResponse', {
-                    success: false
-                });
-            }
+  socket.on("signIn", function(data) {
+    isValidPassword(data, function(res) {
+      if (res) {
+        Player.onConnect(socket);
+        socket.emit("signInResponse", {
+          success: true
         });
-    });
-    socket.on('signUp', function (data) {
-        isUsernameTaken(data, function (res) {
-            if (res) {
-                socket.emit('signUpResponse', {
-                    success: false
-                });
-            } else {
-                addUser(data, function () {
-                    socket.emit('signUpResponse', {
-                        success: true
-                    });
-                });
-            }
+      } else {
+        socket.emit("signInResponse", {
+          success: false
         });
+      }
     });
-
-
-    socket.on('disconnect', function () {
-        delete SOCKET_LIST[socket.id];
-        Player.onDisconnect(socket);
+  });
+  socket.on("signUp", function(data) {
+    isUsernameTaken(data, function(res) {
+      if (res) {
+        socket.emit("signUpResponse", {
+          success: false
+        });
+      } else {
+        addUser(data, function() {
+          socket.emit("signUpResponse", {
+            success: true
+          });
+        });
+      }
     });
-    socket.on('sendMsgToServer', function (data) {
-        var playerName = ("" + socket.id).slice(2, 7);
-        for (var i in SOCKET_LIST) {
-            SOCKET_LIST[i].emit('addToChat', playerName + ': ' + data);
-        }
-    });
+  });
 
-    socket.on('evalServer', function (data) {
-        if (!DEBUG)
-            return;
-        var res = eval(data);
-        socket.emit('evalAnswer', res);
-    });
+  socket.on("disconnect", function() {
+    delete SOCKET_LIST[socket.id];
+    Player.onDisconnect(socket);
+  });
+  socket.on("sendMsgToServer", function(data) {
+    var playerName = ("" + socket.id).slice(2, 7);
+    for (var i in SOCKET_LIST) {
+      SOCKET_LIST[i].emit("addToChat", playerName + ": " + data);
+    }
+  });
 
-
-
+  socket.on("evalServer", function(data) {
+    if (!DEBUG) return;
+    var res = eval(data);
+    socket.emit("evalAnswer", res);
+  });
 });
 
-setInterval(function () {
-    var pack = {
-        player: Player.update(),
-        // bullet: Bullet.update(),
-    }
+setInterval(function() {
+  var pack = {
+    player: Player.update()
+  };
 
-    for (var i in SOCKET_LIST) {
-        var socket = SOCKET_LIST[i];
-        socket.emit('newPositions', pack);
-    }
+  for (var i in SOCKET_LIST) {
+    var socket = SOCKET_LIST[i];
+    socket.emit("newPositions", pack);
+  }
 }, 1000 / frames);
